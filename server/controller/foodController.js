5252b2975fee7382d6b81115229ad96296a9b373
Stephanie Fu <StephaniefuU@gmail.com>
2017-07-31 12:18:57 -0700

    finished controllers

--- server/controller/foodController.js
+++ server/controller/foodController.js
@@ -3,18 +3,51 @@ const Food = require('../../db/models/model');
 
 module.exports = {
   addFood: (req, res) => {
-
+    Food.post({
+      item: req.body.item,
+      expiration: req.body.expiration
+    })
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(404).send(err)
+    })
   },
   getAllFoods: (req, res) => {
-
+    Food.findAll({})
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(404).send(err)
+    })
   },
   findExpiration: (req, res) => {
-
+    Food.findAll({ where: {expiration: req.params.expiration}})
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(404).send(err)
+    })
   },
   findFood: (req, res) => {
-
+    Food.findAll({ where: {item: req.params.item}})
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(404).send(err)
+    })
   },
   deleteFood: (req, res) => {
-
+    Food.destroy({ where: {id: req.params.id}})
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(404).send(err)
+    })
   }
 }
